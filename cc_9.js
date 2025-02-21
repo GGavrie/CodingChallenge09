842484682e75d69748aebad2b39a64f8f59861d1
Gavrie Grant <gavrie@usf.edu>
2025-02-21 10:30:49 -0500

    Task 2 - Created Manager Class with Inheritance

--- cc_9.js
+++ cc_9.js
@@ -15,3 +15,20 @@ class Employee {
         return this.salary * 12;
     }
 }
+
+
+// Task 2 - Creating Manager Class
+function Manager(name, id, department, salary, teamSize) {
+    Employee.call(this, name, id, department, salary); //  Employee properties
+    this.teamSize = teamSize; // Number of employees 
+
+    // Overriding getDetails to include team size
+    this.getDetails = function() {
+        return "Manager: " + this.name + ", ID: " + this.id + ", Department: " + this.department + ", Salary: $" + this.salary + ", Team Size: " + this.teamSize;
+    };
+
+    // Method to calculate bonus (10% of annual salary)
+    this.calculateBonus = function() {
+        return this.calculateAnnualSalary() * 0.10;
+    };
+}
